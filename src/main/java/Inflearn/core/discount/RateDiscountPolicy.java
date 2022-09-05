ac42436e4287a823ba299218a55c68f7b89a1b94
imsehwan <dla612@naver.com>
2022-09-05 20:46:22 +0900

    feat : 새로운 할인 정책 개발

---
+++ src/main/java/Inflearn/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package Inflearn.core.discount;
+
+import Inflearn.core.member.Grade;
+import Inflearn.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
