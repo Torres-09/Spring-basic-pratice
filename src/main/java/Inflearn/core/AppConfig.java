3ebdeaa9586810df27e712e63e9258a921315442
Torres-09 <dla612@naver.com>
2022-09-09 15:14:36 +0900

    refactor : AppConfig 리팩터링

--- src/main/java/Inflearn/core/AppConfig.java
+++ src/main/java/Inflearn/core/AppConfig.java
@@ -1,5 +1,6 @@
 package Inflearn.core;
 
+import Inflearn.core.discount.DiscountPolicy;
 import Inflearn.core.discount.FixDiscountPolicy;
 import Inflearn.core.member.MemberService;
 import Inflearn.core.member.MemberServiceImpl;
@@ -9,10 +10,18 @@ import Inflearn.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
