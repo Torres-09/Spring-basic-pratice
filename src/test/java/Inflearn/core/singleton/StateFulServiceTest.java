8c143a29a349a424e85d4a968a912e469d8c813d
Torres-09 <dla612@naver.com>
2022-09-13 01:12:51 +0900

    test : 싱글톤 stateful , stateless

---
+++ src/test/java/Inflearn/core/singleton/StateFulServiceTest.java
@@ -0,0 +1,36 @@
+package Inflearn.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateFulServiceTest {
+    @Test
+    void stateFulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StateFulService stateFulService1 = ac.getBean(StateFulService.class);
+        StateFulService stateFulService2 = ac.getBean(StateFulService.class);
+
+        // threadA : A 사용자 10000원 주문
+        stateFulService1.order("userA", 10000);
+        // threadB : B 사용자 20000원 주문
+        stateFulService2.order("userA", 20000);
+        //ThreadA : 사용자A 주문 금액 조회
+        int price = stateFulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(stateFulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+        @Bean
+        public StateFulService stateFulService() {
+            return new StateFulService();
+        }
+    }
+
+}
