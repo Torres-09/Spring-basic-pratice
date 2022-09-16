01e29abbf6a89395afdc4fd5e0fab75d251c238e
Torres-09 <dla612@naver.com>
2022-09-17 02:30:03 +0900

    test : 생성자 주입 테스트

---
+++ src/test/java/Inflearn/core/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package Inflearn.core.order;
+
+import Inflearn.core.discount.FixDiscountPolicy;
+import Inflearn.core.member.Grade;
+import Inflearn.core.member.Member;
+import Inflearn.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    public void createOrder() throws Exception{
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L,"name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+}
